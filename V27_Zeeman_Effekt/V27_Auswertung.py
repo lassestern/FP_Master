6d58bd8cc7274223735bc57871d395b87fb81363
Lasse Sternemann <lasse.sternemann@tu-dortmund.de>
2021-11-24 10:10:45 +0100

    Daten

--- V27_Zeeman_Effekt/V27_Auswertung.py
+++ V27_Zeeman_Effekt/V27_Auswertung.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
 import numpy as np
 import scipy as scipy
 import matplotlib.pyplot as plt
@@ -5,6 +12,7 @@ import scipy.constants as const
 from scipy.signal import find_peaks 
 import uncertainties.unumpy as unp 
 from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
+from scipy.optimize import curve_fit
 
 plt.rc('font', size=14, weight='normal')
 #mpl.rcParams['font.sans-serif'] = 'Arial'
@@ -12,8 +20,6 @@ plt.rcParams['legend.fontsize'] = 14
 plt.rc('xtick', labelsize=14) 
 plt.rc('ytick', labelsize=14) 
 
-
-# Zunächst den Kontrast in Abhängigkeit der Polarisationsrichtung des Laserstrahls bestimmen
 
 
 #Formel für die Standardabweichung des Mittelwerts
@@ -29,18 +35,27 @@ def stanni(Zahlen, Mittelwert):
 def linear(x, m, b):
     return m*x+b
 
+I, B = np.genfromtxt("data/magnetfeld.dat", unpack = "True") #I in Ampere, B in mT
 
-I, B = np.genfromtxt("data\magnetfeld.dat") #I in Ampere, B in mT
+B = B*0.001 #B in Tesla
+
+params, cov_params = curve_fit(linear, I, B)
 
 print(I, B)
 
-#fig, axs = plt.subplots(figsize=(7,6))
-#axs.plot(druck, noms(n_gs)**2, "x", label="Messwerte")
-#axs.set_xlabel("Druck [bar]")
-#axs.set_ylabel("n²")
-#axs.plot(x, linear(x, *n_quad_params), label="Fit")
-#axs.legend(loc="best")
-#plt.savefig("n_quad_fit.pdf")
+
+x = np.linspace(0, 7.2)
+
+
+fig, axs = plt.subplots(figsize=(7,6))
+axs.plot(I, B*1000, "x", label="B-Feld")
+axs.set_xlabel("I [A]")
+axs.set_ylabel("B [mT]")
+axs.plot(x, linear(x, *params)*1000, label="Fit")
+axs.legend(loc="best")
+plt.savefig("magnetfeld.pdf")
+
+
 #
 #
 #n_norm = unp.sqrt(1+ 1.013*295.05*n_quad_params_unc[0]/288.15)
